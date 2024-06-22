d133a47cd996077215e173485226ab08401296af
Aqsa_Ejaz <164430228+aqsaejaz001@users.noreply.github.com>
2024-06-22 11:46:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let user_answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+let words = user_answer.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is: ${words.length}`);
